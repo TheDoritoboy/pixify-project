48a5a8868273e7bb466cf59256ca0fa4d3839af2
Kenny <doritoboy.ekk@gmail.com>
2023-05-23 18:23:06 -0700

    uyh

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ const imageSets = [
         'https://images.unsplash.com/photo-1531494391841-6ac2ef3859b8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
         'https://images.unsplash.com/photo-1550358864-518f202c02ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'
       ]
-    }
+    },
     {
       id:5,
       name: 'Ephraem Kruger',
